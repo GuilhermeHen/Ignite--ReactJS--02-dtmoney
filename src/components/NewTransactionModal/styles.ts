72980ee021243ec827860e0255205b5fa5743efe
Guilherme Henrique <guilherme.alvs@hotmail.com>
2021-03-28 21:09:24 -0300

    Cores dos botoes

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -56,13 +56,24 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33cc95',
+  red: '#e52e4d'
+};
+
+export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
 
-    background: transparent;
+    background: ${ (props) => props.isActive ? transparentize(0.9, colors[props.activeColor]) : 'transparent'};
 
     display: flex;
     align-items: center;
@@ -85,5 +96,4 @@ export const TransactionTypeContainer = styled.div`
     margin-left: 1rem;
     font-size: 1rem;
     color: var(--text-title);
-  }
 `;
